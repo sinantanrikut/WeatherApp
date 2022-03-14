55d43ed5ac5b5e58955c9ba6e155772882fe424c
Sinan Tanrıkut <sinantanrikut7@gmail.com>
2022-03-14 16:47:13 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Sinan Tanrıkut on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
