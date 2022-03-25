db9472b016faf50e1b0f0aaf78af5eb413005756
Sinan Tanrıkut <sinantanrikut7@gmail.com>
2022-03-26 02:56:55 +0300

    dati convert işlemleri

--- WeatherApp/Services/Forecast.swift
+++ WeatherApp/Services/Forecast.swift
@@ -18,9 +18,64 @@ class Forecast{
     // Her obje için tek referans olması için hiçbir zaman kendini çoklamaz
     static var forecast = [Forecast]()
     
+  //setter
+    
+    var date:String{
+        if _date == nil {
+            _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "\(currentDate)"
+        
+        
+        return _date
+    }
+    var weatherType:String{
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    var highTemp:String{
+        if _highTemp == nil {
+            _highTemp = ""
+        }
+        return _highTemp
+    }
+    var lowTemp:String{
+        if _lowTemp == nil {
+            _lowTemp = ""
+        }
+        return _lowTemp
+    }
     init(){}//defaul const
     
     init(weatherDict:JSON){
+        _lowTemp = weatherDict["main"]["temp_min"].stringValue
+        _highTemp = weatherDict["main"]["temp_max"].stringValue
+        _weatherType = weatherDict["weather"][0]["main"].stringValue
         
+        
+        //Convert işlemleri
+        let date = weatherDict["dt"].doubleValue
+        let unixConvertedDate = Date(timeIntervalSince1970: date)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.timeStyle = .none
+        _date = unixConvertedDate.dayOfWeek()
+        
+    }
+}
+
+extension Date{
+    func dayOfWeek() -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: self)
     }
 }
