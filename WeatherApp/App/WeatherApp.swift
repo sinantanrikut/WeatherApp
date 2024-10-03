b1350b745bca864c4e6bb7cfdcbb43a0f59f425b
sinanecodation <sinantanrikut7@gmail.com>
2024-10-03 16:25:48 +0300

    add firebase configuration

--- WeatherApp/App/WeatherApp.swift
+++ WeatherApp/App/WeatherApp.swift
@@ -6,10 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    if let _ = FirebaseApp.app() {
+      print("Firebase configuration successful")
+    } else {
+      print("Firebase configuration failed")
+    }
+
+    return true
+  }
+}
 
 @main
 struct WeatherApp: App {
-    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
 //    @StateObject private var loginViewModel = LoginViewModel()
 //    @StateObject private var appTheme = AppTheme()
 //    @StateObject private var userViewModel = UserViewModel()
@@ -27,6 +44,7 @@ struct WeatherApp: App {
                         }
                     }
             } else {
+                
                 ContentView()
                     .preferredColorScheme(.light)
 //                    .environmentObject(loginViewModel)
